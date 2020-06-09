333e9a06da08711a6b705f37ec1e512d9481c1f8
eddyko <eddyko@192.168.21.1>
2020-06-08 20:51:25 -0400

    v2

--- src/main/java/com/example/herokudemo/AFwebService.java
+++ src/main/java/com/example/herokudemo/AFwebService.java
@@ -14,7 +14,7 @@ import com.afweb.service.ServiceAFweb;
 public class AFwebService extends ServiceAFweb{
 //for Dev109    
 //public static String webPrefix ="/web";
-//public static String localTimerURL = "http://127.0.0.1:8084";
+//public static String localTimerURL = "http://127.0.0.1:8082";
 //for Dev110
 public static String webPrefix ="";
 public static String localTimerURL = "http://127.0.0.1:8080"; 
