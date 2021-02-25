dcd5c0bc56c2f1ba911ca741014637437025e6c8
eddy <eddy.ko00@yahoo.ca>
2021-02-25 14:55:45 -0500

    1

--- src/main/java/com/afweb/nnprocess/NN1ProcessByTrend.java
+++ src/main/java/com/afweb/nnprocess/NN1ProcessByTrend.java
@@ -541,7 +541,7 @@ public class NN1ProcessByTrend {
             long LastServUpdateTimer = System.currentTimeMillis();
             int timeout = 15;
             if (ServiceAFweb.processNeuralNetFlag == true) {
-                timeout = timeout * 2;
+                timeout = timeout * 4;
             }
             long lockDate5Min = TimeConvertion.addMinutes(LastServUpdateTimer, timeout); // add 3 minutes
 
