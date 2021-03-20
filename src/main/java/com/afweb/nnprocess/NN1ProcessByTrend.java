8db4ef6f5896da1af8490592309eb0404f475653
eddyko <eddyko@eddyko-PC>
2021-03-20 13:25:48 -0400

    v2

--- src/main/java/com/afweb/nnprocess/NN1ProcessByTrend.java
+++ src/main/java/com/afweb/nnprocess/NN1ProcessByTrend.java
@@ -78,8 +78,9 @@ public class NN1ProcessByTrend {
         NeuralNetAllStockInputTesting(serviceAFWeb, ConstantKey.INT_TR_MACD0);
         logger.info("> processAllStockInputNeuralNetTrend TR MACD1... ");
         NeuralNetAllStockInputTesting(serviceAFWeb, ConstantKey.INT_TR_MACD1);
-
-        NeuralNetAllStockNN30CreatJava(serviceAFWeb, ConstantKey.TR_NN30);
+        if (CKey.NN_DATA_DB == false) {
+            NeuralNetAllStockNN30CreatJava(serviceAFWeb, ConstantKey.TR_NN30);
+        }
         logger.info("> processAllStockInputNeuralNetTrend TR NN1 end....... ");
 
     }
