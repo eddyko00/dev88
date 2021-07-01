f020a52fd4d0c4e3875b9f6ada01aa8d324238db
eddyko <eddyko@eddyko-PC>
2021-07-01 14:21:27 -0400

    v2

--- src/main/java/com/afweb/nnsignal/TradingSignalProcess.java
+++ src/main/java/com/afweb/nnsignal/TradingSignalProcess.java
@@ -909,7 +909,7 @@ public class TradingSignalProcess {
                     }
                     // udpate tr SubStatus to open
                     // need to get the latest TR object after the SystemAddTransactionOrder
-                    trObj = servicegetAccountStockIDByTRStockIDSystemtockID(accountObj.getId(), stock.getId(), trObj.getTrname());
+                    trObj = serviceAFWeb.getAccountStockIDByTRStockIDSystem(accountObj.getId(), stock.getId(), trObj.getTrname());
                     // need to get the latest TR object after the SystemAddTransactionOrder
                     trObj.setSubstatus(ConstantKey.OPEN);
                     String updateSQL = AccountDB.SQLUpdateAccountStockStatus(trObj);
