341c5c19c953ad660b176fdf5efac4b8eb25d7cc
eddyko <eddyko@eddyko-PC>
2021-06-30 21:49:32 -0400

    v2

--- src/main/java/com/example/herokudemo/ControllerNN.java
+++ src/main/java/com/example/herokudemo/ControllerNN.java
@@ -21,7 +21,6 @@ import java.util.ArrayList;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
